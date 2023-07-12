c3ee9bd2ecb2cbd9d30ef240af51055a4875ed16
tman6290 <tofaratifarinu@gmail.com>
2023-07-12 02:56:54 +0100

    I made the nav bar sticky. Now it follows the user as they scroll through the page.

--- ArtPortflioWebsite/js/script.js
+++ ArtPortflioWebsite/js/script.js
@@ -16,4 +16,9 @@ window.onscroll = () => {
             });
         };
     });
+
+    /*sticky navbar*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
 };
